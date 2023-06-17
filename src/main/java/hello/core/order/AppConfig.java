3d263bafe31d2846ae72609ff85f713334112fb3
seungjaejeon <wjstmdwo2000@naver.com>
2023-06-18 01:30:39 +0900

    AppConfig refactoring

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -1,16 +1,23 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
